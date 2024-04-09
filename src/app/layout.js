1f073a2b1e380466c49a1bdd2ea66e345912cfae
Arkadiusz Kadzielawski <akadzielawski@myRCCS.COM>
2024-04-09 14:52:54 -0400

    here we go again

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <form name="contact" data-netlify="true" hidden>
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <textarea name="message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </body>
     </html>
   );
 }
