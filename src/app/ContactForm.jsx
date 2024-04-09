23f7d01142321729cd9586678e18f4cd459d764e
Arkadiusz Kadzielawski <akadzielawski@myRCCS.COM>
2024-04-09 14:46:05 -0400

    form form

--- src/app/ContactForm.jsx
+++ src/app/ContactForm.jsx
@@ -38,12 +38,7 @@ const ContactForm = () => {
   return (
     <div className="bg-white p-8 rounded-lg shadow-md w-96 mx-auto">
       <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
-      <form
-        name="contact"
-        data-netlify="true"
-        method="post"
-        onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit}>
         <input type="hidden" name="form-name" value="contact" />
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">
